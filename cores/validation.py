336435e67efd12aab922cc463e479ea3a17a24ea
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-03-29 16:59:52 +0900

    CREATE: 초기 모델 작성

---
+++ cores/validation.py
@@ -0,0 +1,13 @@
+import re
+from django.forms import ValidationError
+
+regex_email             = '^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$'
+regex_password          = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$'
+
+def email_validation(email):
+    if not re.match(regex_email, email):
+        raise ValidationError('INVALID_ERROR')
+        
+def password_validation(password):
+    if not re.match(regex_password, password):
+        raise ValidationError('INVALID_ERROR')
