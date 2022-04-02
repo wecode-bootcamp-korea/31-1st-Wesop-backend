43f354a81789c1797c9c8a32a8d6203daab4f788
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-03 00:09:25 +0900

    MODIFY: offset, limit 수정

--- products/views.py
+++ products/views.py
@@ -11,7 +11,6 @@ class ProductListView(View):
 
         if category_id == 0:
             products = Product.objects.all()[offset:limit]
-
         else:
             products = Product.objects.filter(category=category_id)[offset:limit]
 
