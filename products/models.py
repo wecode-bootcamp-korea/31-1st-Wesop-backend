66bceee3f16ee0c781d3b5ea361994051973e267
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-04 15:11:45 +0900

    [Add] offset, limt 수정

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 
 from users.models import User
@@ -18,15 +19,15 @@ class Product(TimeStamp):
         db_table = 'products'
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=45)
+    category_name    = models.CharField(max_length=45)
     main_description = models.CharField(max_length=1000, null=True)
-    sub_description = models.CharField(max_length=1000, null=True)
+    sub_description  = models.CharField(max_length=1000, null=True)
 
     class Meta:
         db_table = 'categories'
 
 class Ingredient(models.Model):
-    ingredients = models.CharField(max_length=300)
+    name = models.CharField(max_length=300)
 
     class Meta:
         db_table = 'ingredients'
@@ -40,14 +41,14 @@ class ProductIngredient(models.Model):
         db_table = 'product_ingredients'
 
 class ProductImage(models.Model):
-    image_url = models.CharField(max_length=2000)
-    product   = models.ForeignKey('Product', on_delete=models.CASCADE)
+    url     = models.CharField(max_length=2000)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'product_imgaes'
     
 class SkinType(models.Model):
-    skin_type = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
 
     class Meta:
         db_table = 'skin_types'
@@ -58,15 +59,6 @@ class ProductSkintype(models.Model):
 
     class Meta:
         db_table = 'product_skintypes'
-
-class ProductDescription(models.Model):
-    list_description  = models.CharField(max_length=100)
-    nomal_description = models.CharField(max_length=100)
-    nomal_title = models.CharField(max_length=100, null=True)
-    category          = models.ForeignKey('Category', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'product_descriptions'
 
 
 class Review(TimeStamp):
--- products/views.py
+++ products/views.py
@@ -1,31 +1,35 @@
 from django.views import View
 from django.http  import JsonResponse
+from django.db.models import Q
 
 from products.models import *
 
 class ProductListView(View):
     def get(self, request):
-        category_id = int(request.GET.get('category_id', 0))
-        offset = int(request.GET.get('offset', 0))
-        limit = int(request.GET.get('limit', 0))
-
-        if category_id == 0:
+        category_id = request.GET.get('category_id', None)
+        offset      = int(request.GET.get('offset', 0))
+        limit       = int(request.GET.get('limit', 0))
+            
+        if category_id == None:
             products = Product.objects.all()[offset:limit]
             
         else:
             products = Product.objects.filter(category=category_id)[offset:limit]
 
         result = [{
-            'id': product.id,
-            'badge':product.badge,  
-            'productName':product.name,
-            'size':product.size,
-            'price':product.price,
-            'url':[img.image_url for img in product.productimage_set.all()],
-            'category' : {
-                'categoryId':product.category.id,
-                'categoryName':product.category.category_name,
-                'categoryDescription':product.category.main_description
+            'id'         : product.id,
+            'badge'      : product.badge,
+            'productName': product.name,
+            'size'       : product.size,
+            'price'      : product.price,
+            'feeling'    : product.feeling,
+            'ingredient' : [item.ingredient.name for item in product.productingredient_set.all()],
+            'skin_type'  : [productskintype.skin_type.name for productskintype in product.skintypes.all()],
+            'url'        : [img.url for img in product.productimage_set.all()],
+            'category'   : {
+                'categoryId'         : product.category.id,
+                'categoryName'       : product.category.category_name,
+                'categoryDescription': product.category.main_description
             }
         } for product in products]
         return JsonResponse({'result':result}, status=200)
