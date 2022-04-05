8231f8e885fd52a8346e63756a98fb3007934498
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-04-05 11:51:05 +0900

    [Modify]: 멘토님 피드백 수정

--- products/views.py
+++ products/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.http  import JsonResponse
 from django.views import View
 
@@ -11,10 +9,10 @@ class RecommendedView(View):
         try:
             category_id         = Product.objects.get(id = product_id).category
             recommend_list = [{
-                'name'      : value.name,
-                'image'     : [image.url for image in value.productimage_set.all()],
-                'skintype'  : [types.skin_type.name for types in value.productskintype_set.all()]
-                } for value in Product.objects.filter(category = category_id).exclude(id=product_id)]
+                'name'      : product.name,
+                'image'     : [image.url for image in product.productimage_set.all()],
+                'skintype'  : [types.skin_type.name for types in product.productskintype_set.all()]
+                } for product in Product.objects.filter(category = category_id).exclude(id=product_id)]
 
             return JsonResponse({'result' : [ recommend_list ] }, status = 200)
         except KeyError:
