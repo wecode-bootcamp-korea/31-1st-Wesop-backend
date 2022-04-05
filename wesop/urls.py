4f75eb90dd5995c37b98f99d63d0a909c561a2eb
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-05 12:33:29 +0900

    MODIFY: url 수정

--- wesop/urls.py
+++ wesop/urls.py
@@ -2,9 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
-<<<<<<< HEAD
-    path('products', include('products.urls'))
-=======
     path("products",include('products.urls'))
->>>>>>> main
 ]
