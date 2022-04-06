0a68d1f6eb15754155105868fbcdec03937f5cf4
leesan195159 <sniping1997@naver.com>
2022-04-06 19:57:55 +0900

    create: 장바구니 patch 기능 구현

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+from products.models import Product
 
 from cores.utils import author
 from carts.models import Cart
@@ -28,5 +29,26 @@ class CartView(View):
 
         except Cart.DoesNotExist:
             return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=400)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+    @author
+    def patch(self, request):
+        try:
+            data       = json.loads(request.body)
+            user       = request.user
+            product_id = data['product_id']
+            quantity   = data['quantity']
+
+            if quantity <= 0 or quantity >= 21:
+                return JsonResponse({'message': 'INVALID_QUANTITY'}, status=400)
+
+            if Cart.objects.filter(product_id=product_id, user_id=user).exists():
+                cart          = Cart.objects.get(user=user, product_id=product_id)
+                cart.quantity = data['quantity']
+                cart.save()
+                return JsonResponse({'message': 'QUANTITY_CHANGED'}, status=201)
+            return JsonResponse({'message': 'PRODUCT_DOES_NOT_EXIT'}, status=404)
+
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
