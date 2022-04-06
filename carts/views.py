c7cca1966dbad140df065b0530f11758cea83695
leesan195159 <sniping1997@naver.com>
2022-04-06 20:04:23 +0900

    create: 장바구니 delete 기능 구현

--- carts/views.py
+++ carts/views.py
@@ -22,7 +22,7 @@ class CartView(View):
                 cart.quantity += 1
                 cart.save()
             else:
-                return JsonResponse({'message': 'QUANTITY_CHANGED'}, status=200)
+                return JsonResponse({'message': 'INVALID_QUANTITY'}, status=200)
 
             return JsonResponse({'message': 'CART_CREATED'}, status=201)
 
@@ -30,3 +30,23 @@ class CartView(View):
             return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=400)
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+    @author
+    def delete(self, request):
+        try:
+            cart_id = request.GET.getlist('cart_id')
+            user = request.user
+
+            if cart_id==[]:
+                return JsonResponse({'message': 'LIST_EMPTY'}, status=400)
+
+            for cart in cart_id:
+                if not Cart.objects.filter(id=cart).exists():
+                    return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=404)
+                Cart.objects.filter(id=cart).delete()
+            return JsonResponse({'message': 'CART_DELETED'}, status=200)
+
+        except Cart.DoesNotExist:
+            return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=404)
+        except ValueError:
+            return JsonResponse({'message': 'VALUE_ERROR'}, status=400)
