662fa0d724671e937fb0e9a5ae2511ec4b3e443a
leesan195159 <sniping1997@naver.com>
2022-04-06 18:18:24 +0900

    modify: 장바구니 Post 기능 수정

--- carts/views.py
+++ carts/views.py
@@ -12,21 +12,19 @@ class CartView(View):
     @author
     def post(self, request):
         try:
-            data       = json.loads(request.body)
-            user       = request.user
-            product_id = data['product_id']
+            data = json.loads(request.body)
 
             cart, created = Cart.objects.get_or_create(
-                user       =user,
-                product_id =product_id
+                user       = request.user,
+                product_id = data['product_id']
             )
-            if created:
-                return JsonResponse({'message': 'CART_CREATED'}, status=201)
-            if cart.quantity > 19:
-                return JsonResponse({'message': 'INVALID_QUANTITY'}, status=200)
-            cart.quantity += 1
-            cart.save()
-            return JsonResponse({'message': 'QUANTITY_CHANGED'}, status=200)
+            if not created and cart.quantity < 20:
+                cart.quantity += 1
+                cart.save()
+            else:
+                return JsonResponse({'message': 'QUANTITY_CHANGED'}, status=200)
+
+            return JsonResponse({'message': 'CART_CREATED'}, status=201)
 
         except Cart.DoesNotExist:
             return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=400)
